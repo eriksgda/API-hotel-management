b905c95684bbe39f2f87bc9dc9fbc9f811187f6c
eriksgda <erik.ksgda@gmail.com>
2025-05-27 11:45:18 -0300

    feat: add login endpoint to auth controller

--- src/main/java/com/eriksgda/hotel_management/controller/auth/AuthController.java
+++ src/main/java/com/eriksgda/hotel_management/controller/auth/AuthController.java
@@ -1,5 +1,7 @@
 package com.eriksgda.hotel_management.controller.auth;
 
+import com.eriksgda.hotel_management.model.AuthRequestDTO;
+import com.eriksgda.hotel_management.model.AuthResponseDTO;
 import com.eriksgda.hotel_management.model.SignupResponseDTO;
 import com.eriksgda.hotel_management.model.SignupRequestDTO;
 import com.eriksgda.hotel_management.service.auth.AuthService;
@@ -8,6 +10,8 @@ import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +24,7 @@ public class AuthController {
     private final AuthService authService;
 
     @Autowired
-    public AuthController(AuthServiceImpl authService) {
+    public AuthController(AuthServiceImpl authService, AuthenticationManager authenticationManager) {
         this.authService = authService;
     }
 
@@ -29,11 +33,24 @@ public class AuthController {
         try {
             SignupResponseDTO response = this.authService.createUser(data);
 
-            return ResponseEntity.status(HttpStatus.OK).body(response);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (EntityExistsException exception) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody AuthRequestDTO data) {
+        try {
+            AuthResponseDTO response = this.authService.createAuthToken(data);
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (BadCredentialsException exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
         }
     }
 }
