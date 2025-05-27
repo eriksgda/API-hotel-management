3c23ab1515a9a20ba013d2f919f950b51eea4f14
eriksgda <erik.ksgda@gmail.com>
2025-05-27 11:52:28 -0300

    feat: add UserService and UserServiceImpl with loadUserByUsername for user authentication

---
+++ src/main/java/com/eriksgda/hotel_management/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.eriksgda.hotel_management.service;
+
+import com.eriksgda.hotel_management.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findFirstByEmail(username).orElseThrow( () ->
+                    new UsernameNotFoundException("Email not found."));
+            }
+        };
+    }
+}
