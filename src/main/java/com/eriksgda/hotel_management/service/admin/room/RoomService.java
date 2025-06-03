085ebd3d19fb476a00058b60a61c11f4473fe9a7
eriksgda <erik.ksgda@gmail.com>
2025-06-02 21:01:31 -0300

    feat: add get all rooms method

--- src/main/java/com/eriksgda/hotel_management/service/admin/room/RoomService.java
+++ src/main/java/com/eriksgda/hotel_management/service/admin/room/RoomService.java
@@ -2,7 +2,9 @@ package com.eriksgda.hotel_management.service.admin.room;
 
 import com.eriksgda.hotel_management.model.CreateRoomRequestDTO;
 import com.eriksgda.hotel_management.model.CreateRoomResponseDTO;
+import com.eriksgda.hotel_management.model.RoomsResponseDTO;
 
 public interface RoomService {
     CreateRoomResponseDTO createRoom(CreateRoomRequestDTO createRoomRequestDTO);
+    RoomsResponseDTO getAllRooms(Integer pageNumber);
 }
