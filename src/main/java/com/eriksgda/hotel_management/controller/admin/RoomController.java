9ca39de96e9eadf14531accf1271f6d2725803aa
eriksgda <erik.ksgda@gmail.com>
2025-06-02 21:03:11 -0300

    feat: refactor controller url to api/admin/rooms/ and add get all rooms endpoint with path variable

--- src/main/java/com/eriksgda/hotel_management/controller/admin/RoomController.java
+++ src/main/java/com/eriksgda/hotel_management/controller/admin/RoomController.java
@@ -2,17 +2,15 @@ package com.eriksgda.hotel_management.controller.admin;
 
 import com.eriksgda.hotel_management.model.CreateRoomRequestDTO;
 import com.eriksgda.hotel_management.model.CreateRoomResponseDTO;
+import com.eriksgda.hotel_management.model.RoomsResponseDTO;
 import com.eriksgda.hotel_management.service.admin.room.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/admin/room/")
+@RequestMapping("api/admin/rooms/")
 public class RoomController {
 
     private final RoomService roomService;
@@ -30,6 +28,16 @@ public class RoomController {
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (IllegalArgumentException exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
+    }
+
+    @GetMapping("/all/{pageNumber}")
+    public ResponseEntity<?> getAllRooms(@PathVariable Integer pageNumber) {
+        try {
+            RoomsResponseDTO response = this.roomService.getAllRooms(pageNumber);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
         }
