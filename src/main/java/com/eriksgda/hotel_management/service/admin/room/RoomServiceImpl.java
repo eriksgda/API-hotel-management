acc074071feec325b20b59e8bf326c86c7236b07
eriksgda <erik.ksgda@gmail.com>
2025-06-02 21:01:47 -0300

    feat: create get all rooms method with page

--- src/main/java/com/eriksgda/hotel_management/service/admin/room/RoomServiceImpl.java
+++ src/main/java/com/eriksgda/hotel_management/service/admin/room/RoomServiceImpl.java
@@ -3,9 +3,17 @@ package com.eriksgda.hotel_management.service.admin.room;
 import com.eriksgda.hotel_management.entity.Room;
 import com.eriksgda.hotel_management.model.CreateRoomRequestDTO;
 import com.eriksgda.hotel_management.model.CreateRoomResponseDTO;
+import com.eriksgda.hotel_management.model.RoomsResponseDTO;
 import com.eriksgda.hotel_management.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class RoomServiceImpl implements RoomService{
@@ -41,5 +49,16 @@ public class RoomServiceImpl implements RoomService{
         Room newRoom = this.roomRepository.save(room);
 
         return CreateRoomResponseDTO.fromEntity(newRoom);
+    }
+
+    @Override
+    public RoomsResponseDTO getAllRooms(Integer pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber, 6);
+        Page<Room> rooms = this.roomRepository.findAll(pageable);
+
+        return new RoomsResponseDTO(
+                rooms.stream().map(CreateRoomResponseDTO::fromEntity).collect(Collectors.toList()),
+                rooms.getTotalPages(),
+                rooms.getPageable().getPageNumber());
     }
 }
