2f8069dedcb3c01997807571ae85098de54c924d
eriksgda <erik.ksgda@gmail.com>
2025-06-03 15:50:33 -0300

    refactor: update RoomsResponseDTO to use RoomResponseDTO instead of CreateRoomResponseDTO

--- src/main/java/com/eriksgda/hotel_management/model/RoomsResponseDTO.java
+++ src/main/java/com/eriksgda/hotel_management/model/RoomsResponseDTO.java
@@ -2,5 +2,5 @@ package com.eriksgda.hotel_management.model;
 
 import java.util.List;
 
-public record RoomsResponseDTO(List<CreateRoomResponseDTO> RoomsList, Integer totalPages, Integer pageNumber) {
+public record RoomsResponseDTO(List<RoomResponseDTO> RoomsList, Integer totalPages, Integer pageNumber) {
 }
