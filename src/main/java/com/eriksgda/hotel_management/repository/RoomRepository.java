011f40616b5c1ada3b4a2c9694ab041a230c2f94
eriksgda <erik.ksgda@gmail.com>
2025-06-05 22:42:35 -0300

    feat: add find by available method in room repository

--- src/main/java/com/eriksgda/hotel_management/repository/RoomRepository.java
+++ src/main/java/com/eriksgda/hotel_management/repository/RoomRepository.java
@@ -1,6 +1,8 @@
 package com.eriksgda.hotel_management.repository;
 
 import com.eriksgda.hotel_management.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,5 @@ import java.util.UUID;
 @Service
 public interface RoomRepository extends JpaRepository<Room, UUID> {
     boolean existsRoomByName(String name);
+    Page<Room> findByAvailable(boolean available, Pageable pageable);
 }
