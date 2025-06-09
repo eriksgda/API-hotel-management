7e413f7b77e49147db8b8c49e0a7edc457578ccc
eriksgda <erik.ksgda@gmail.com>
2025-06-09 10:25:58 -0300

    feat: add findAllByUserId method in Reservation repository

--- src/main/java/com/eriksgda/hotel_management/repository/ReservationRepository.java
+++ src/main/java/com/eriksgda/hotel_management/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.eriksgda.hotel_management.repository;
 
 import com.eriksgda.hotel_management.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    Page<Reservation> findAllByUserId(UUID userId, Pageable pageable);
 }
