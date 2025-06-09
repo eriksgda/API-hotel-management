c52216f6416d2d39ff1db2b030a83395b003eee3
eriksgda <erik.ksgda@gmail.com>
2025-06-08 22:04:29 -0300

    feat: add change reservation method in reservation service and impl

--- src/main/java/com/eriksgda/hotel_management/service/admin/reservation/ReservationService.java
+++ src/main/java/com/eriksgda/hotel_management/service/admin/reservation/ReservationService.java
@@ -1,7 +1,11 @@
 package com.eriksgda.hotel_management.service.admin.reservation;
 
+import com.eriksgda.hotel_management.model.ReservationResponseDTO;
 import com.eriksgda.hotel_management.model.ReservationsResponseDTO;
+
+import java.util.UUID;
 
 public interface ReservationService {
     ReservationsResponseDTO getAllReservations(int pageNumber);
+    ReservationResponseDTO changeReservationStatus(UUID id, String status);
 }
--- src/main/java/com/eriksgda/hotel_management/service/admin/reservation/ReservationServiceImpl.java
+++ src/main/java/com/eriksgda/hotel_management/service/admin/reservation/ReservationServiceImpl.java
@@ -1,15 +1,20 @@
 package com.eriksgda.hotel_management.service.admin.reservation;
 
 import com.eriksgda.hotel_management.entity.Reservation;
+import com.eriksgda.hotel_management.entity.Room;
+import com.eriksgda.hotel_management.enums.ReservationStatus;
 import com.eriksgda.hotel_management.model.ReservationResponseDTO;
 import com.eriksgda.hotel_management.model.ReservationsResponseDTO;
 import com.eriksgda.hotel_management.repository.ReservationRepository;
+import com.eriksgda.hotel_management.repository.RoomRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,11 +22,15 @@ public class ReservationServiceImpl implements ReservationService{
 
     private final ReservationRepository reservationRepository;
 
+    private final RoomRepository roomRepository;
+
     private static final int SEARCH_RESULT_PER_PAGE = 4;
 
     @Autowired
-    public ReservationServiceImpl(ReservationRepository reservationRepository) {
+    public ReservationServiceImpl(ReservationRepository reservationRepository,
+                                  RoomRepository roomRepository) {
         this.reservationRepository = reservationRepository;
+        this.roomRepository = roomRepository;
     }
 
     @Override
@@ -35,5 +44,27 @@ public class ReservationServiceImpl implements ReservationService{
                 reservationPage.getPageable().getPageNumber(),
                 reservationPage.stream().map(ReservationResponseDTO::fromEntity).collect(Collectors.toList())
         );
+    }
+
+    @Override
+    public ReservationResponseDTO changeReservationStatus(UUID id, String status) {
+        Reservation reservation = reservationRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Reservation doesn't exists.")
+        );
+
+        if (ReservationStatus.APPROVED.name().equalsIgnoreCase(status)) {
+            reservation.setReservationStatus(ReservationStatus.APPROVED);
+        } else {
+            reservation.setReservationStatus(ReservationStatus.REJECTED);
+        }
+
+        Reservation reservationChanged = this.reservationRepository.save(reservation);
+
+        Room room = reservationChanged.getRoom();
+        room.setAvailable(false);
+
+        this.roomRepository.save(room);
+
+        return ReservationResponseDTO.fromEntity(reservationChanged);
     }
 }
