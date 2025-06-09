e776217dd4cab37f64c37a06f261e920fe2b579b
eriksgda <erik.ksgda@gmail.com>
2025-06-09 10:26:41 -0300

    feat: add getAllReservationsByUserId method in customer booking service and impl

--- src/main/java/com/eriksgda/hotel_management/service/customer/booking/BookingService.java
+++ src/main/java/com/eriksgda/hotel_management/service/customer/booking/BookingService.java
@@ -2,7 +2,11 @@ package com.eriksgda.hotel_management.service.customer.booking;
 
 import com.eriksgda.hotel_management.model.ReservationRequestDTO;
 import com.eriksgda.hotel_management.model.ReservationResponseDTO;
+import com.eriksgda.hotel_management.model.ReservationsResponseDTO;
+
+import java.util.UUID;
 
 public interface BookingService {
     ReservationResponseDTO createReservation(ReservationRequestDTO reservationRequestDTO);
+    ReservationsResponseDTO getAllReservationsByUserId(UUID id, int pageNumber);
 }
--- src/main/java/com/eriksgda/hotel_management/service/customer/booking/BookingServiceImpl.java
+++ src/main/java/com/eriksgda/hotel_management/service/customer/booking/BookingServiceImpl.java
@@ -6,14 +6,20 @@ import com.eriksgda.hotel_management.entity.User;
 import com.eriksgda.hotel_management.enums.ReservationStatus;
 import com.eriksgda.hotel_management.model.ReservationRequestDTO;
 import com.eriksgda.hotel_management.model.ReservationResponseDTO;
+import com.eriksgda.hotel_management.model.ReservationsResponseDTO;
 import com.eriksgda.hotel_management.repository.ReservationRepository;
 import com.eriksgda.hotel_management.repository.RoomRepository;
 import com.eriksgda.hotel_management.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class BookingServiceImpl implements BookingService{
@@ -23,6 +29,8 @@ public class BookingServiceImpl implements BookingService{
     private final UserRepository userRepository;
 
     private final RoomRepository roomRepository;
+
+    private static final int SEARCH_RESULT_PER_PAGE = 4;
 
     @Autowired
     public BookingServiceImpl(ReservationRepository reservationRepository,
@@ -57,5 +65,18 @@ public class BookingServiceImpl implements BookingService{
         }
 
         throw new IllegalArgumentException("error");
+    }
+
+    @Override
+    public ReservationsResponseDTO getAllReservationsByUserId(UUID id, int pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber, SEARCH_RESULT_PER_PAGE);
+
+        Page<Reservation> reservationPage = reservationRepository.findAllByUserId(id, pageable);
+
+        return new ReservationsResponseDTO(
+                reservationPage.getTotalPages(),
+                reservationPage.getPageable().getPageNumber(),
+                reservationPage.stream().map(ReservationResponseDTO::fromEntity).collect(Collectors.toList())
+        );
     }
 }
