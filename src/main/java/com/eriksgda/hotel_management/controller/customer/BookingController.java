ec40db65db8993f7dd0802692b661620268ee282
eriksgda <erik.ksgda@gmail.com>
2025-06-09 10:27:14 -0300

    feat: add getAllReservationsByUserId endpoint in customer booking controller

--- src/main/java/com/eriksgda/hotel_management/controller/customer/BookingController.java
+++ src/main/java/com/eriksgda/hotel_management/controller/customer/BookingController.java
@@ -2,11 +2,14 @@ package com.eriksgda.hotel_management.controller.customer;
 
 import com.eriksgda.hotel_management.model.ReservationRequestDTO;
 import com.eriksgda.hotel_management.model.ReservationResponseDTO;
+import com.eriksgda.hotel_management.model.ReservationsResponseDTO;
 import com.eriksgda.hotel_management.service.customer.booking.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/customer")
@@ -24,6 +27,18 @@ public class BookingController {
         try {
             ReservationResponseDTO response = this.bookingService.createReservation(data);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
+    }
+
+    @GetMapping("/rooms/reservation/all/{pageNumber}/{id}")
+    public ResponseEntity<?> getAllReservationsByUserId(@PathVariable int pageNumber, @PathVariable UUID id) {
+        try {
+            ReservationsResponseDTO response = this.bookingService.getAllReservationsByUserId(id, pageNumber);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         } catch (IllegalArgumentException exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         } catch (Exception exception) {
